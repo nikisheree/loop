d6f97f09be27f11f0162aa1365cfb4362bce6f6c
nikisheree <155241874+nikisheree@users.noreply.github.com>
2024-09-21 22:26:00 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,197 @@
+package niki;
+import java.util.*;
+
+// class otter{
+//     public void caller(){
+//         for(int j = 0; j<=10; j++){
+//             System.out.println("OM Vasu Dhare Swah ha");
+//         }
+//     }
+// }
+
+// class cult{
+//     public void meeeeeta(){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter the number");
+//         int j = sc.nextInt();
+
+//         for(int i =1; i<=j; i++){
+//             for(int b=j; b>i; b--){
+//                 System.out.print(" $ ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// class cold{
+//     public void vurr(){
+
+//     for(int j = 0; j<=10; j++){
+//         System.out.println("shshshshshshshs");
+//     }
+//     }
+// }
+
+
+// class botom{
+//     public void cutaaa(){
+//     for(int j=10; j>=0; j--){
+//         System.out.print("T ");
+//     }
+//     }
+// }
+// class cutter{
+//     public void vim(){
+//         for(int j =10; j>=0; j--){
+//             System.out.println("tuitutu");
+//         }
+//     }
+// }
+
+// class fut{
+
+//     public void youtubr(){
+//         for (int j = 0; j<=20; j++){
+//             System.out.println("fulller");
+//         }
+//     }
+// }
+
+// class bult{
+//     public void itt(){
+//         for(int i =10; i>=0; i--){
+//             System.out.println(i);
+//         }
+
+//     }
+// }
+
+// class gutt{
+
+//     public void male(){
+
+//         for(int t =10; t >=0; t--){
+// //here "t" is a literal and i have gave the value to t is 10 after that
+// //the value will be print from 10 to 0. it will like 10,9,8,7,6,5,4,3,2,1,0
+// //when the 0 matches to the 0 the loop will end there. 
+// //will perform this using decrement operator
+
+//         }
+//     }
+// }
+
+// class tub{
+//     public void curl(){
+//         for(int g =10; g>=0; g--){
+//             for(int y=0; y<=g; y++ ){
+//                 System.out.print(" * ");
+//             }
+//             System.out.println( );
+//         }
+//     }
+// }
+
+
+
+// class man{
+//     public void str(){
+//         for(int j=0; j<=10; j++){
+//             //In the first for loop i have give the value to the j intger 10 after that
+//             for(int b =0; b<=j; b++){
+
+//                 System.out.print("T");
+//             }
+//             System.out.println( );
+//         }
+//     }
+// }
+
+// class peter{
+//     public void ruter(){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("welcome to java");
+//         int j = sc.nextInt();
+//         System.out.println("enter the number");
+//         for(int h=0; h<=j; h++){
+//             for(int l=0; l<=h; l++){
+//                 System.out.print(" * ");
+//             }
+//             System.out.println( );
+//         }
+//     }
+// }
+
+// class cute{
+// public void softter(){
+//     System.out.println(" Hey welcome to the page ");
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("enter the number that to print in pyramid");
+//     int gurl = sc.nextInt();
+//     for(int t = 0; t<=gurl; t++){
+//         for(int gf =0; gf<=t; gf++){
+//             System.out.print(" N D ");
+//         }
+//         System.out.println( );
+//     }
+// }
+// }
+
+// class cum{
+//     public  void mule(){
+//         for(int f=10; f>=0; f++){
+//             System.out.println(" u ");
+//         }
+
+//     }
+// }
+
+// class massage {
+//     public void rut(){
+//         System.out.println(" Jater");
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println(" reverse star pattern");
+//     int j =  sc.nextInt();
+//     for(int g=10 ; g>=0; g--){
+//         for(int kur =10; kur<=g; kur++){
+//             System.out.print(" * ");
+//         }
+//         System.out.println( );
+//     }
+//     }
+// }
+
+// class nmer{
+//     public void matter(){
+//         Scanner sc = new Scanner(System.in);
+//         int h = sc.nextInt();
+//         for(int j =0; j<=h; j++ ){
+// System.out.println(j);
+//         }
+//     }
+// }
+
+
+class cutter{
+    public void gut(){
+int [] number ={1,2,3,4,5,6,7,8};
+for(int j =0; j<= number.length; j++){
+
+System.out.println(number[j]);
+
+}
+    }
+}
+public class loops {
+    public static void main(String[] args) {
+
+cutter obj = new cutter();
+obj.gut();
+
+
+
+    }
+    
+}
+                                                                                        
